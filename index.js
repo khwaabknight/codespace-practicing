934d90ceb2fdbdf46ee2df1384d0446d36414fa2
khwaabknight <89650360+khwaabknight@users.noreply.github.com>
2023-11-16 08:48:03 +0000

    set up a server

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Aman Codespace");
+require('dotenv').config()
+const app = require("./app");
+const {PORT} = process.env
+app.listen(PORT,() => {
+    console.log(`Server is running at port : ${PORT}`);
+});
